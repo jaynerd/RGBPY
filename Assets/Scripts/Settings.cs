b0681cdbaa24d00c8c7a61c055aa454d52b40b7a
jaynerd <j4yn3rd@gmail.com>
2018-04-18 23:13:04 +1200

    hexagon rotation

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+	public static float hexagonRotateSpeed = 40f;
+	public static float hexagonRotateAcceleration = 1f;
+}
